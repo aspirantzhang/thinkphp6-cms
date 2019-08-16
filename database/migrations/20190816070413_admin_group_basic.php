205717c6dbc93be5dfb951aeb171433e28aa3f5e
aspirantzhang <admin@aspirantzhang.com>
2019-08-16 15:19:45 +0800

    admin group database design

---
+++ database/migrations/20190816070413_admin_group_basic.php
@@ -0,0 +1,41 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class AdminGroupBasic extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $group = $this->table('auth_group', ['signed' => false, 'engine' => 'InnoDB', 'collation' => 'utf8mb4_unicode_ci']);
+        $group->addColumn('name', 'string', ['limit' => 255])
+              ->addColumn('rules', 'string', ['limit' => 255])
+              ->addColumn('create_time', 'datetime')
+              ->addColumn('update_time', 'datetime')
+              ->addColumn('delete_time', 'datetime', ['null' => true])
+              ->addColumn('status', 'boolean', ['default' => 1])
+              ->addIndex(['name'], ['unique' => true])
+              ->create();
+    }
+}
