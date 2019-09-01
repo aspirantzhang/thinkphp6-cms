a6c60be6eabe5402b22814e6dca2405c41d50a01
aspirantzhang <admin@aspirantzhang.com>
2019-09-01 13:32:39 +0800

    auth control basic

---
+++ app/middleware/BackendAuth.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\middleware;
+
+use myttyy\Auth;
+
+
+class BackendAuth
+{
+
+    protected $noNeedAuth = [
+        'backend/index/login',
+        'backend/admin/index',
+        'backend/admin/create',
+        'backend/admin/save',
+        'backend/admin/read',
+        'backend/admin/edit',
+        'backend/admin/update',
+        'backend/admin/delete',
+        'backend/admin/groups',
+        'backend/auth_group/index',
+        'backend/auth_group/create',
+        'backend/auth_group/save',
+        'backend/auth_group/read',
+        'backend/auth_group/edit',
+        'backend/auth_group/update',
+        'backend/auth_group/delete',
+        'backend/auth_rule/index',
+        'backend/auth_rule/create',
+        'backend/auth_rule/save',
+        'backend/auth_rule/read',
+        'backend/auth_rule/edit',
+        'backend/auth_rule/update',
+        'backend/auth_rule/delete',
+    ];
+
+    public function handle($request, \Closure $next)
+    {
+
+        $appName = parse_name($request->app());
+        $controllerName = parse_name($request->controller());
+        $actionName = parse_name($request->action());
+        $fullPath = $appName.'/'.$controllerName.'/'.$actionName;
+
+        if (in_array($fullPath, $this->noNeedAuth)) {
+            return $next($request);
+        } else {
+
+            $auth = new Auth;
+
+            if ($auth->check($fullPath, 1)) {
+                return $next($request);
+            } else {
+
+                $data = [
+                    'status'    =>  'error',
+                    'msg'       =>  'No permission.',
+                ];
+                return json($data)->header([
+                    'access-control-allow-origin' => 'http://localhost:8000',
+                    'access-control-allow-methods' => 'GET, POST, PATCH, PUT, DELETE',
+                    'access-control-allow-headers' => 'Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With',
+                    'access-control-allow-credentials' => 'true',
+                ]);
+            }
+        }
+
+
+    }
+}
