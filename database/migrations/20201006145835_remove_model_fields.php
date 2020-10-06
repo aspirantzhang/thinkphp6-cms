82372a3fb5f23c75a46ae482c3052c747c61dc23
aspirantzhang <admin@aspirantzhang.com>
2020-10-06 23:50:19 +0800

    feat: exchange fields in table rule & model

---
+++ database/migrations/20201006145835_remove_model_fields.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DbMigrations;
+
+use Phinx\Migration\AbstractMigration;
+
+final class RemoveModelFields extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $model = $this->table('model');
+        $model->removeColumn('icon')
+                ->removeColumn('path')
+                ->removeColumn('component')
+                ->removeColumn('access')
+                ->update();
+                
+        $rule = $this->table('auth_rule');
+        $rule->addColumn('icon', 'string', ['limit' => 255, 'after' => 'condition'])
+                ->addColumn('path', 'string', ['limit' => 255, 'after' => 'icon'])
+                ->addColumn('component', 'string', ['limit' => 255, 'after' => 'path'])
+                ->addColumn('access', 'string', ['limit' => 255, 'after' => 'component'])
+                ->update();
+    }
+}
