3b7e6ed0b667cfa2daf328cf1a18c8a94f63b243
aspirantzhang <admin@aspirantzhang.com>
2020-11-05 15:11:18 +0800

    feat: add api doc of Model

--- app/backend/controller/Model.php
+++ app/backend/controller/Model.php
@@ -18,21 +18,283 @@ class Model extends Common
         $this->model = new ModelService();
         parent::initialize();
     }
+    /** @OA\Schema(
+    *    schema="model-list",
+    *    @OA\Property(
+    *        property="success",
+    *        type="boolean",
+    *    ),
+    *    @OA\Property(
+    *        property="message",
+    *        type="string",
+    *    ),
+    *    @OA\Property(
+    *        property="data",
+    *        type="object",
+    *        @OA\Property(
+    *            property="page",
+    *            type="object",
+    *            allOf={
+    *               @OA\Schema(
+    *                   @OA\Property(
+    *                       property="title",
+    *                       type="string",
+    *                   ),
+    *                   @OA\Property(
+    *                       property="type",
+    *                       type="string",
+    *                   ),
+    *                   @OA\Property(
+    *                       property="searchBar",
+    *                       type="boolean",
+    *                   ),
+    *                   @OA\Property(
+    *                       property="trash",
+    *                       type="boolean",
+    *                   ),
+    *               ),
+    *            },
+    *        ),
+    *        @OA\Property(
+    *            property="layout",
+    *            type="object",
+    *            allOf={
+    *               @OA\Schema(
+    *                   @OA\Property(
+    *                       property="tableColumn",
+    *                       type="object",
+    *                   ),
+    *                   @OA\Property(
+    *                       property="tableToolBar",
+    *                       type="object",
+    *                   ),
+    *                   @OA\Property(
+    *                       property="batchToolBar",
+    *                       type="object",
+    *                   ),
+    *               ),
+    *            },
+    *        ),
+    *        @OA\Property(
+    *            property="dataSource",
+    *            type="array",
+    *            @OA\Items(ref="#/components/schemas/model"),
+    *        ),
+    *        @OA\Property(
+    *            property="meta",
+    *            type="object",
+    *            allOf={
+    *               @OA\Schema(
+    *                   @OA\Property(
+    *                       property="total",
+    *                       type="number",
+    *                   ),
+    *                   @OA\Property(
+    *                       property="per_page",
+    *                       type="number",
+    *                   ),
+    *                   @OA\Property(
+    *                       property="page",
+    *                       type="number",
+    *                   ),
+    *               ),
+    *            },
+    *        ),
+    *    ),
+    * )
+    */
+ 
+    /** @OA\Schema(
+    *    schema="model",
+    *    @OA\Property(
+    *        property="id",
+    *        type="integer",
+    *        format="int32",
+    *    ),
+    *    @OA\Property(
+    *        property="title",
+    *        type="string",
+    *    ),
+    *    @OA\Property(
+    *        property="name",
+    *        type="string",
+    *    ),
+    *    @OA\Property(
+    *        property="data",
+    *        type="string",
+    *    ),
+    *    @OA\Property(
+    *        property="create_time",
+    *        type="string",
+    *    ),
+    *    @OA\Property(
+    *        property="status",
+    *        type="integer",
+    *        example=1,
+    *        @OA\Items(
+    *            type="integer",
+    *            enum = {0, 1},
+    *        )
+    *    ),
+    * )
+    */
 
+    /**
+    * @OA\Get(
+    *     path="/backend/models",
+    *     summary="Get the list of models.",
+    *     tags={"models"},
+    *     security={
+    *       {"ApiKeyAuth": {}}
+    *     },
+    *     @OA\Parameter(
+    *         name="sort",
+    *         in="query",
+    *         description="The field used for sort.",
+    *         @OA\Schema(
+    *             type="array",
+    *             @OA\Items(
+    *                 type="string",
+    *                 enum = {"id", "create_time"},
+    *             )
+    *         )
+    *     ),
+    *     @OA\Parameter(
+    *         name="order",
+    *         in="query",
+    *         description="The method used for sorting.",
+    *         @OA\Schema(
+    *             type="array",
+    *             @OA\Items(
+    *                 type="string",
+    *                 enum = {"descent", "ascent"},
+    *             )
+    *         )
+    *     ),
+    *     @OA\Parameter(
+    *         name="status",
+    *         in="query",
+    *         description="0 = Disabled, 1 = Enabled",
+    *         @OA\Schema(
+    *             type="integer",
+    *             example=1,
+    *             @OA\Items(
+    *                 type="integer",
+    *                 enum = {0, 1},
+    *             )
+    *         )
+    *     ),
+    *     @OA\Response(
+    *        response="200",
+    *        description="Success",
+    *        @OA\MediaType(
+    *            mediaType="application/json",
+    *            @OA\Schema(ref="#/components/schemas/model-list"),
+    *        )
+    *     ),
+    *     @OA\Response(
+    *        response="200.1",
+    *        description="Need Login",
+    *        @OA\MediaType(
+    *            mediaType="application/json",
+    *            @OA\Schema(ref="#/components/schemas/error"),
+    *        )
+    *     ),
+    * )
+    */
     public function home()
     {
         $result = $this->model->paginatedListAPI($this->request->only($this->model->getAllowHome()));
 
         return $this->json(...$result);
     }
-
+    /**
+    * @OA\Get(
+    *     path="/backend/models/add",
+    *     summary="Get the page for adding a new model.",
+    *     tags={"models"},
+    *     security={
+    *       {"ApiKeyAuth": {}}
+    *     },
+    *     @OA\Response(
+    *         response=200,
+    *         description="Success"
+    *     ),
+    * )
+    */
     public function add()
     {
         $result = $this->model->addAPI();
 
         return $this->json(...$result);
     }
-
+    /**
+    * @OA\Post(
+    *     path="/backend/models",
+    *     summary="Used for adding a new model.",
+    *     tags={"models"},
+    *     security={
+    *       {"ApiKeyAuth": {}}
+    *     },
+    *     @OA\RequestBody(
+    *         required=true,
+    *         @OA\MediaType(
+    *             mediaType="application/json",
+    *             @OA\Schema(
+    *                 type="object",
+    *                 required={
+    *                     "title",
+    *                     "name",
+    *                 },
+    *                 example={
+    *                     "title": "User Title",
+    *                     "name": "user",
+    *                     "create_time": "2020-11-02T16:11:24+08:00",
+    *                     "status": true,
+    *                 },
+    *                 @OA\Property(
+    *                     property="title",
+    *                     type="string",
+    *                 ),
+    *                 @OA\Property(
+    *                     property="name",
+    *                     type="string",
+    *                 ),
+    *                 @OA\Property(
+    *                     property="create_time",
+    *                     type="string",
+    *                 ),
+    *                 @OA\Property(
+    *                     property="status",
+    *                     type="boolean",
+    *                 ),
+    *             )
+    *         )
+    *     ),
+    *     @OA\Response(
+    *        response="200",
+    *        description="Success",
+    *        @OA\MediaType(
+    *            mediaType="application/json",
+    *            @OA\Schema(
+    *                type="object",
+    *                @OA\Property(
+    *                    property="success",
+    *                    type="boolean",
+    *                ),
+    *                @OA\Property(
+    *                    property="message",
+    *                    type="string",
+    *                ),
+    *                @OA\Property(
+    *                    property="data",
+    *                    type="object",
+    *                )
+    *            )
+    *        )
+    *     ),
+    * )
+    */
     public function save()
     {
         $tableName = strtolower($this->request->param('name'));
@@ -102,21 +364,152 @@ class Model extends Common
 
         return $this->json(...$result);
     }
-
+    /**
+    * @OA\Get(
+    *     path="/backend/models/{id}",
+    *     summary="Get information of a specific model.",
+    *     tags={"models"},
+    *     security={
+    *       {"ApiKeyAuth": {}}
+    *     },
+    *     @OA\Parameter(
+    *         name="id",
+    *         in="path",
+    *         required=true,
+    *         @OA\Schema(type="integer")
+    *     ),
+    *     @OA\Response(
+    *        response="200",
+    *        description="Success",
+    *     ),
+    * )
+    */
     public function read($id)
     {
         $result = $this->model->readAPI($id);
 
         return $this->json(...$result);
     }
-
+    /**
+    * @OA\Put(
+    *     path="/backend/models/{id}",
+    *     summary="Update the information of a specific model.",
+    *     tags={"models"},
+    *     security={
+    *       {"ApiKeyAuth": {}}
+    *     },
+    *     @OA\Parameter(
+    *         name="id",
+    *         in="path",
+    *         required=true,
+    *         @OA\Schema(type="integer")
+    *     ),
+    *     @OA\RequestBody(
+    *         required=true,
+    *         @OA\MediaType(
+    *             mediaType="application/json",
+    *             @OA\Schema(
+    *                 type="object",
+    *                 required={
+    *                     "title",
+    *                     "name",
+    *                 },
+    *                 example={
+    *                     "title": "User Title",
+    *                     "name": "user",
+    *                     "create_time": "2020-11-02T16:11:24+08:00",
+    *                     "status": true,
+    *                 },
+    *                 @OA\Property(
+    *                     property="title",
+    *                     type="string",
+    *                 ),
+    *                 @OA\Property(
+    *                     property="name",
+    *                     type="string",
+    *                 ),
+    *                 @OA\Property(
+    *                     property="create_time",
+    *                     type="string",
+    *                 ),
+    *                 @OA\Property(
+    *                     property="status",
+    *                     type="boolean",
+    *                 ),
+    *             )
+    *         )
+    *     ),
+    *     @OA\Response(
+    *        response="200",
+    *        description="Success",
+    *        @OA\MediaType(
+    *            mediaType="application/json",
+    *            @OA\Schema(
+    *                type="object",
+    *                @OA\Property(
+    *                    property="success",
+    *                    type="boolean",
+    *                ),
+    *                @OA\Property(
+    *                    property="message",
+    *                    type="string",
+    *                ),
+    *                @OA\Property(
+    *                    property="data",
+    *                    type="object",
+    *                )
+    *            )
+    *        )
+    *     ),
+    * )
+    */
     public function update($id)
     {
         $result = $this->model->updateAPI($id, $this->request->only($this->model->getAllowUpdate()));
 
         return $this->json(...$result);
     }
-
+    /**
+    * @OA\Delete(
+    *     path="/backend/models",
+    *     summary="Delete a model.",
+    *     tags={"models"},
+    *     security={
+    *       {"ApiKeyAuth": {}}
+    *     },
+    *     @OA\RequestBody(
+    *         required=true,
+    *         @OA\MediaType(
+    *             mediaType="application/json",
+    *             @OA\Schema(
+    *                 type="object",
+    *                 required={
+    *                     "type",
+    *                     "ids",
+    *                 },
+    *                 example={
+    *                     "type": "delete",
+    *                     "ids": {53, 54, 55},
+    *                 },
+    *                 @OA\Property(
+    *                     property="type",
+    *                     type="string",
+    *                     enum={"delete","deletePermanently"}
+    *                 ),
+    *                 @OA\Property(
+    *                     property="ids",
+    *                     type="array",
+    *                     @OA\Items(),
+    *                 ),
+    *             )
+    *         )
+    *     ),
+    *     @OA\Response(
+    *        response="200",
+    *        description="Success",
+    *     ),
+    * )
+    */
     public function delete()
     {
         $result = $this->model->deleteAPI($this->request->param('ids'), $this->request->param('type'));
@@ -160,21 +553,130 @@ class Model extends Common
         
         return $this->json(...$result);
     }
-
+    /**
+    * @OA\Post(
+    *     path="/backend/models/restore",
+    *     summary="Restore a model from the trash can.",
+    *     tags={"models"},
+    *     security={
+    *       {"ApiKeyAuth": {}}
+    *     },
+    *     @OA\RequestBody(
+    *         required=true,
+    *         @OA\MediaType(
+    *             mediaType="application/json",
+    *             @OA\Schema(
+    *                 type="object",
+    *                 required={
+    *                     "ids",
+    *                 },
+    *                 example={
+    *                     "ids": {53, 54, 55},
+    *                 },
+    *                 @OA\Property(
+    *                     property="ids",
+    *                     type="array",
+    *                     @OA\Items(),
+    *                 ),
+    *             )
+    *         )
+    *     ),
+    *     @OA\Response(
+    *        response="200",
+    *        description="Success",
+    *     ),
+    * )
+    */
     public function restore()
     {
         $result = $this->model->restoreAPI($this->request->param('ids'));
         
         return $this->json(...$result);
     }
-    
+    /**
+    * @OA\Get(
+    *     path="/backend/models/design/{id}",
+    *     summary="Get design data of a specific model.",
+    *     tags={"models"},
+    *     security={
+    *       {"ApiKeyAuth": {}}
+    *     },
+    *     @OA\Parameter(
+    *         name="id",
+    *         in="path",
+    *         required=true,
+    *         @OA\Schema(type="integer")
+    *     ),
+    *     @OA\Response(
+    *        response="200",
+    *        description="Success",
+    *     ),
+    * )
+    */
     public function design($id)
     {
         $result = $this->model->designAPI($id);
 
         return $this->json(...$result);
     }
-    
+    /**
+    * @OA\Put(
+    *     path="/backend/models/design/{id}",
+    *     summary="Update design data of a specific model.",
+    *     tags={"models"},
+    *     security={
+    *       {"ApiKeyAuth": {}}
+    *     },
+    *     @OA\Parameter(
+    *         name="id",
+    *         in="path",
+    *         required=true,
+    *         @OA\Schema(type="integer")
+    *     ),
+    *     @OA\RequestBody(
+    *         required=true,
+    *         @OA\MediaType(
+    *             mediaType="application/json",
+    *             @OA\Schema(
+    *                 type="object",
+    *                 required={
+    *                     "data",
+    *                 },
+    *                 example={
+    *                     "data": {}
+    *                 },
+    *                 @OA\Property(
+    *                     property="data",
+    *                     type="object",
+    *                     @OA\Items()
+    *                 ),
+    *             )
+    *         )
+    *     ),
+    *     @OA\Response(
+    *        response="200",
+    *        description="Success",
+    *        @OA\MediaType(
+    *            mediaType="application/json",
+    *            @OA\Schema(
+    *                type="object",
+    *                @OA\Property(
+    *                    property="success",
+    *                    type="boolean",
+    *                ),
+    *                @OA\Property(
+    *                    property="message",
+    *                    type="string",
+    *                ),
+    *                @OA\Property(
+    *                    property="data",
+    *                    type="object",
+    *                )
+    *            )
+    *        )
+    *     ),
+    * )
+    */
     public function designUpdate($id)
     {
         $tableName = ModelService::where('id', $id)->value('name');
