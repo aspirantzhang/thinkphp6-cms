ad6162985d0f5dc98681f3cab054a456c6f65afb
aspirantzhang <admin@aspirantzhang.com>
2021-01-21 14:34:06 +0800

    refactor: add delete_time to built-in field

--- app/backend/config/field.php
+++ app/backend/config/field.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'allowHome' => ['sort', 'order', 'page', 'per_page', 'id', 'create_time', 'status', 'trash'],
-    'allowList' => ['id', 'create_time', 'status'],
+    'allowHome' => ['sort', 'order', 'page', 'per_page', 'id', 'create_time', 'delete_time', 'status', 'trash'],
+    'allowList' => ['id', 'create_time', 'delete_time', 'status'],
     'allowSort' => ['sort', 'order', 'id', 'create_time'],
     'allowRead' => ['id', 'create_time', 'update_time', 'status'],
     'allowSave' => ['create_time', 'status'],
     'allowUpdate' => ['create_time', 'status'],
-    'allowSearch' => ['id', 'create_time', 'status'],
+    'allowSearch' => ['id', 'create_time', 'delete_time', 'status'],
 ];
