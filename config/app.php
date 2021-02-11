936a11862c892e93914d5843e93b6f6f22767a15
aspirantzhang <admin@aspirantzhang.com>
2021-02-11 20:59:02 +0800

    feat: add http 404 & 500 prod error template

--- config/app.php
+++ config/app.php
@@ -29,6 +29,11 @@ return [
     // 异常页面的模板文件
     'exception_tmpl'   => app()->getThinkPath() . 'tpl/think_exception.tpl',
 
+    'http_exception_template'    =>  [
+        404 =>  \think\facade\App::getAppPath() . '404.json',
+        500 =>  \think\facade\App::getAppPath() . '500.json',
+    ],
+
     // 错误显示信息,非调试模式有效
     'error_message'    => '页面错误！请稍后再试～',
     // 显示错误信息
