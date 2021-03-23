4e761853ac54be2e79577bbfa06d27961058a04f
aspirantzhang <admin@aspirantzhang.com>
2021-03-23 23:17:49 +0800

    feat: add routeName validate rule

--- app/api/validate/Model.php
+++ app/api/validate/Model.php
@@ -11,8 +11,8 @@ class Model extends Validate
         'id' => 'require|number',
         'ids' => 'require|numberArray',
         'title' => 'require|length:2,32',
-        'table_name' => 'require|length:2,32',
-        'route_name' => 'require|length:2,32',
+        'table_name' => 'require|length:2,10|checkRouteName',
+        'route_name' => 'require|length:2,10|checkRouteName',
         'status' => 'numberTag',
         'page' => 'number',
         'per_page' => 'number',
@@ -27,9 +27,11 @@ class Model extends Validate
         'title.require' => 'The model title field is empty.',
         'title.length' => 'Model title length should be between 2 and 32.',
         'table_name.require' => 'The table name field is empty.',
-        'table_name.length' => 'Table name length should be between 2 and 32.',
+        'table_name.length' => 'Table name length should be between 2 and 10.',
+        'table_name.checkRouteName' => 'Invalid table name.',
         'route_name.require' => 'The route name field is empty.',
-        'route_name.length' => 'Route name length should be between 2 and 32.',
+        'route_name.length' => 'Route name length should be between 2 and 10.',
+        'route_name.checkRouteName' => 'Invalid route name.',
         'status.numberTag' => 'Invalid status format.',
         'page.number' => 'Page must be numbers only.',
         'per_page.number' => 'Per_page must be numbers only.',
@@ -53,5 +55,14 @@ class Model extends Validate
         $this->only(['page', 'per_page', 'id', 'status', 'create_time'])
             ->remove('id', 'require')
             ->remove('create_time', 'require');
+    }
+
+    protected function checkRouteName($value, $rule, $data = [])
+    {
+        if (preg_match("/^[a-zA-z0-9_-]+$/i", $value) == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
