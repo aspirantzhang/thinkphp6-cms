77cb5a718dcee7b615feb6619b2e046c16abbc05
aspirantzhang <admin@aspirantzhang.com>
2021-04-05 14:10:11 +0800

    test: fix code coverage

--- tests/api/TestCase.php
+++ tests/api/TestCase.php
@@ -2,10 +2,13 @@
 
 namespace tests\api;
 
+use think\App;
+
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
     protected function setUp(): void
     {
+        (new App())->http->run();
     }
     protected function tearDown(): void
     {
