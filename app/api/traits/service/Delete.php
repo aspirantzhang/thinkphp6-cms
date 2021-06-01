401be4670ddbbbdeda8be362638256598143aa2f
aspirantzhang <admin@aspirantzhang.com>
2021-06-01 14:38:10 +0800

    refactor: improve deleteAPI

--- app/api/traits/service/Delete.php
+++ app/api/traits/service/Delete.php
@@ -25,21 +25,37 @@ trait Delete
                 $allIds = array_merge($allIds, getDescendantSet('id', 'id', $id, $tree));
             }
 
-            if ($type === 'deletePermanently') {
-                $dataSet = $this->withTrashed()->whereIn('id', array_unique($allIds))->select();
-                if (!$dataSet->isEmpty()) {
-                    $body = $dataSet->toArray();
-                    foreach ($dataSet as $item) {
-                        $this->deleteI18nData($item->id);
+            $dataSet = $this->withTrashed()->whereIn('id', array_unique($allIds))->select();
+            if (!$dataSet->isEmpty()) {
+                $body = $dataSet->toArray();
+                foreach ($dataSet as $item) {
+                    if ($type === 'deletePermanently') {
                         $item->force()->delete();
+                        $this->deleteI18nData($item->id);
+                    } else {
+                        $item->delete();
                     }
-                    $result = true;
-                } else {
-                    return $this->error('Nothing to do.');
                 }
+                $result = true;
             } else {
-                $result = $this->withTrashed()->whereIn('id', array_unique($allIds))->select()->delete();
+                return $this->error('Nothing to do.');
             }
+
+            // if ($type === 'deletePermanently') {
+            //     $dataSet = $this->withTrashed()->whereIn('id', array_unique($allIds))->select();
+            //     if (!$dataSet->isEmpty()) {
+            //         $body = $dataSet->toArray();
+            //         foreach ($dataSet as $item) {
+            //             $this->deleteI18nData($item->id);
+            //             $item->force()->delete();
+            //         }
+            //         $result = true;
+            //     } else {
+            //         return $this->error('Nothing to do.');
+            //     }
+            // } else {
+            //     $result = $this->withTrashed()->whereIn('id', array_unique($allIds))->select()->delete();
+            // }
             
             if ($result) {
                 return $this->success('Delete successfully.', $body);
