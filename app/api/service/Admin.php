e9a54a660637d37bd42d0e362b2f0ef6798cd7f1
aspirantzhang <admin@aspirantzhang.com>
2021-07-24 22:50:49 +0800

    perf: improve loginAPI logic

--- app/api/service/Admin.php
+++ app/api/service/Admin.php
@@ -11,17 +11,15 @@ class Admin extends AdminLogic
     public function loginAPI($params = [])
     {
         $admin = $this->where('admin_name', $params['username'])->find();
-        if ($admin) {
-            if (password_verify($params['password'], $admin->password)) {
-                $data = $admin->visible(['id', 'admin_name'])->toArray();
-                $addition = [
-                    'currentAuthority' => 'admin',
-                    'type' => $params['type'] ?? null
-                ];
-                return $this->success('', $data, [], $addition);
-            }
+        if ($admin && password_verify($params['password'], $admin->getAttr('password'))) {
+            $data = $admin->visible(['id', 'admin_name'])->toArray();
+            $addition = [
+                'currentAuthority' => 'admin',
+                'type' => $params['type'] ?? null
+            ];
+            return $this->success('', $data, [], $addition);
         }
-
+        
         return $this->error(__('incorrect username or password'));
     }
 }
