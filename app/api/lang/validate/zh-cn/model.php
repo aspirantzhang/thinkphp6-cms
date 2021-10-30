c7d7af6c42d8a7d5a305487b2d35e544d5ac2b72
aspirantzhang <admin@aspirantzhang.com>
2021-10-30 12:42:05 +0800

    refactor(Model): increase the length of model title and name to 64

--- app/api/lang/validate/zh-cn/model.php
+++ app/api/lang/validate/zh-cn/model.php
@@ -2,9 +2,9 @@
 
 return [
     'model@model_title#require' => '模型标题 不可为空',
-    'model@model_title#length:2,32' => '模型标题 长度范围必须为 2 - 32',
-    'model@table_name#require' => '模型名称 不可为空',
-    'model@table_name#length:2,10' => '模型名称 长度范围必须为 2 - 10',
+    'model@model_title#length:2,64' => '模型标题 长度范围必须为 2 - 64',
+    'model@table_name#require' => '模型表名 不可为空',
+    'model@table_name#length:2,64' => '模型表名 长度范围必须为 2 - 64',
     'model@table_name#checkTableName' => '无效的值: 模型表名',
     'model@type#numberTag' => '无效的值：类型',
     'model@parent_id#requireIf' => '分类模型必须选择所属上级',
