10ca796b1598e8561cce53427c265b91ccb9362f
aspirantzhang <admin@aspirantzhang.com>
2022-01-04 22:36:29 +0800

    feat: add category to built-in allow field

--- config/api/common/field.php
+++ config/api/common/field.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'allowHome' => ['id', 'parent_id', 'title', 'create_time', 'delete_time', 'status', 'trash', 'page', 'per_page', 'sort', 'order'],
+    'allowHome' => ['id', 'parent_id', 'category', 'title', 'create_time', 'delete_time', 'status', 'trash', 'page', 'per_page', 'sort', 'order'],
     'allowSort' => ['sort', 'order', 'id', 'create_time', 'list_order'],
-    'allowRead' => ['id', 'parent_id', 'title', 'pathname', 'list_order', 'create_time', 'update_time', 'status'],
-    'allowSave' => [ 'parent_id', 'title', 'pathname', 'list_order', 'create_time', 'status'],
-    'allowUpdate' => ['title',  'parent_id', 'pathname', 'list_order', 'create_time', 'status'],
+    'allowRead' => ['id', 'parent_id', 'title', 'pathname', 'category', 'list_order', 'create_time', 'update_time', 'status'],
+    'allowSave' => [ 'parent_id', 'title', 'pathname', 'category', 'list_order', 'create_time', 'status'],
+    'allowUpdate' => ['title', 'parent_id', 'pathname', 'category', 'list_order', 'create_time', 'status'],
     'allowTranslate' => ['title']
 ];
