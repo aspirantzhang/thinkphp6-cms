fab352bf1faafae4f9c483310af11d06f6136db5
aspirantzhang <admin@aspirantzhang.com>
2022-03-05 21:10:45 +0800

    feat: add model property in CoreFacade

--- app/core/facade/CoreFacade.php
+++ app/core/facade/CoreFacade.php
@@ -8,5 +8,20 @@ use app\core\Facade;
 
 abstract class CoreFacade extends Facade
 {
+    protected $model;
     public bool $isNull = false;
+
+    public function __construct()
+    {
+        $this->initModel();
+    }
+
+    protected function initModel()
+    {
+        $modelClass = str_replace('facade', 'model', static::class);
+        if (class_exists($modelClass)) {
+            $this->model = new $modelClass();
+            return;
+        }
+    }
 }
