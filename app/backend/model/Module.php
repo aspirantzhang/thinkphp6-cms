8bf590f4ec383a8d62e2d51460c76f2c90d1984c
aspirantzhang <admin@aspirantzhang.com>
2022-03-14 14:05:39 +0800

    refactor: change field name for store operations

--- app/backend/model/Module.php
+++ app/backend/model/Module.php
@@ -6,16 +6,16 @@ namespace app\backend\model;
 
 class Module extends Common
 {
-    protected $json = ['field', 'layout', 'setting'];
+    protected $json = ['field', 'operation', 'setting'];
     protected $jsonAssoc = true;
     public static $config = [
         'titleField' => 'module_title',
         'uniqueValue' => ['module_title', 'table_name'],
         'ignoreFilter' => [],
         'allowHome' => ['module_title', 'table_name', 'type'],
-        'allowRead' => ['module_title', 'table_name', 'type', 'field', 'layout', 'setting'],
-        'allowSave' => ['module_title', 'table_name', 'type', 'field', 'layout', 'setting'],
-        'allowUpdate' => ['field', 'layout', 'setting'],
+        'allowRead' => ['module_title', 'table_name', 'type', 'field', 'operation', 'setting'],
+        'allowSave' => ['module_title', 'table_name', 'type', 'field', 'operation', 'setting'],
+        'allowUpdate' => ['field', 'operation', 'setting'],
         'allowTranslate' => ['module_title'],
         'revisionTable' => [],
     ];
