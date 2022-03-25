eba261e58bd0970027771fc8bd45e3334ae19c3b
aspirantzhang <admin@aspirantzhang.com>
2022-03-25 21:25:43 +0800

    refactor: improve RouterValidate middleware

--- app/middleware/RouterValidate.php
+++ app/middleware/RouterValidate.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace app\middleware;
 
+use app\core\exception\SystemException;
+
 class RouterValidate
 {
     public function handle($request, \Closure $next, $name)
     {
         $appName = parse_name(app('http')->getName());
-        $validateClassName = 'app\\' . $appName . '\\validate\\' . $name;
-        $validateClass = new $validateClassName();
-        $validateClass->failException(true)->scene(parse_name($request->action()))->check($request->param());
+        $validateClass = 'app\\' . $appName . '\\validate\\' . $name;
+
+        if (!class_exists($validateClass)) {
+            throw new SystemException('invalid validate class: ' . $validateClass);
+        }
+
+        (new $validateClass())->failException(true)->scene(parse_name($request->action()))->check($request->param());
 
         return $next($request);
     }
