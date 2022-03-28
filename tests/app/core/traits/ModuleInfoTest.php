b790e4490fe6e32309e988ec80f922102302d290
aspirantzhang <admin@aspirantzhang.com>
2022-03-28 21:38:59 +0800

    test: add more unit tests

--- tests/app/core/traits/ModuleInfoTest.php
+++ tests/app/core/traits/ModuleInfoTest.php
@@ -21,4 +21,103 @@ class ModuleInfoTest extends \tests\TestCase
         $trait->name = 'unitTest';
         $this->assertEquals('unit_test', $trait->getTableName());
     }
+
+    public function testGetFieldSetWithSpecificPropertyDefaultReturn()
+    {
+        $trait = new class() {
+            use ModuleInfo;
+
+            public function getModuleField()
+            {
+                return [];
+            }
+        };
+        $this->assertEquals([], $trait->getFieldSetWithSpecificProperty('whatever'));
+    }
+
+    public function testGetFieldSetWithSpecificPropertyValidReturn()
+    {
+        $trait = new class() {
+            use ModuleInfo;
+
+            public function getModuleField()
+            {
+                return [
+                    [
+                        'name' => 'admin_name',
+                        'type' => 'input',
+                        'unique' => true,
+                        'filter' => true,
+                        'translate' => false,
+                        'position' => 'tab.main',
+                        'order' => 0,
+                        'allow' => [
+                            'browse' => true,
+                            'read' => true,
+                            'add' => true,
+                            'edit' => false,
+                        ],
+                        'validate' => [
+                            'required' => true,
+                            'length' => [
+                                'min' => 0,
+                                'max' => 255,
+                            ],
+                        ],
+                    ],
+                    [
+                        'name' => 'password',
+                        'type' => 'password',
+                        'unique' => false,
+                        'filter' => true,
+                        'translate' => false,
+                        'position' => 'tab.main',
+                        'order' => 0,
+                        'hideInColumn' => true,
+                        'allow' => [
+                            'browse' => false,
+                            'read' => true,
+                            'add' => true,
+                            'edit' => true,
+                        ],
+                        'validate' => [
+                            'required' => true,
+                            'length' => [
+                                'min' => 0,
+                                'max' => 255,
+                            ],
+                        ],
+                    ],
+                    [
+                        'name' => 'display_name',
+                        'type' => 'input',
+                        'unique' => false,
+                        'filter' => true,
+                        'translate' => true,
+                        'position' => 'tab.main',
+                        'order' => 0,
+                        'allow' => [
+                            'browse' => true,
+                            'read' => true,
+                            'add' => true,
+                            'edit' => true,
+                        ],
+                        'validate' => [
+                            'length' => [
+                                'min' => 0,
+                                'max' => 255,
+                            ],
+                        ],
+                    ],
+                ];
+            }
+        };
+        $this->assertEquals([], $trait->getFieldSetWithSpecificProperty('not-exist-property'));
+        $this->assertEquals(['admin_name'], $trait->getFieldSetWithSpecificProperty('unique'));
+        $this->assertEquals(['admin_name', 'password', 'display_name'], $trait->getFieldSetWithSpecificProperty('filter'));
+        $this->assertEquals(['admin_name', 'display_name'], $trait->getFieldSetWithSpecificProperty('allow.browse'));
+        $this->assertEquals(['admin_name', 'password', 'display_name'], $trait->getFieldSetWithSpecificProperty('allow.read'));
+        $this->assertEquals(['password', 'display_name'], $trait->getFieldSetWithSpecificProperty('allow.edit'));
+        $this->assertEquals(['admin_name', 'password', 'display_name'], $trait->getFieldSetWithSpecificProperty('allow.add'));
+    }
 }
