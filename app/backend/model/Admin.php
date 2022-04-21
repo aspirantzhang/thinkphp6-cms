f80a1cb71d436acb2da060e46bc865eb5d1be411
aspirantzhang <admin@aspirantzhang.com>
2022-04-21 21:58:09 +0800

    style: delete file-based config

--- app/backend/model/Admin.php
+++ app/backend/model/Admin.php
@@ -6,15 +6,4 @@ namespace app\backend\model;
 
 class Admin extends Common
 {
-    public static $config = [
-        'titleField' => 'admin_name',
-        'uniqueValue' => ['admin_name'],
-        'ignoreFilter' => [],
-        'allowHome' => ['groups', 'admin_name', 'display_name', 'comment'],
-        'allowRead' => ['admin_name', 'display_name', 'comment'],
-        'allowSave' => ['admin_name', 'password', 'groups', 'display_name', 'comment'],
-        'allowUpdate' => ['password', 'display_name', 'comment', 'groups'],
-        'allowTranslate' => ['display_name', 'comment'],
-        'revisionTable' => ['auth_admin_group'],
-    ];
 }
--- app/backend/model/Module.php
+++ app/backend/model/Module.php
@@ -9,16 +9,4 @@ class Module extends Common
     protected $json = ['field', 'operation', 'setting'];
 
     protected $jsonAssoc = true;
-
-    public static $config = [
-        'titleField' => 'module_title',
-        'uniqueValue' => ['module_title', 'table_name'],
-        'ignoreFilter' => [],
-        'allowHome' => ['module_title', 'table_name', 'type'],
-        'allowRead' => ['module_title', 'table_name', 'type', 'field', 'operation', 'setting'],
-        'allowSave' => ['module_title', 'table_name', 'type', 'field', 'operation', 'setting'],
-        'allowUpdate' => ['field', 'operation', 'setting'],
-        'allowTranslate' => ['module_title'],
-        'revisionTable' => [],
-    ];
 }
