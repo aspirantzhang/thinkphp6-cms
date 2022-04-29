d7d1a2e6a455ec58ecf8ac2f11657ae03ce9b133
aspirantzhang <admin@aspirantzhang.com>
2022-04-29 21:12:05 +0800

    feat: add getPaginatedList for Admin facade

--- app/backend/facade/Admin.php
+++ app/backend/facade/Admin.php
@@ -4,11 +4,33 @@ declare(strict_types=1);
 
 namespace app\backend\facade;
 
-use app\core\facade\CoreFacade;
+use app\core\CoreFacade;
+use app\core\domain\Layout\ListLayout;
+use app\core\mapper\ListData;
 
 class Admin extends CoreFacade
 {
-    public function getPaginatedList(array $fields, array $option)
+    public function getPaginatedList(array $option = [], array $input = null)
     {
+        $input ??= $this->model->getAllowBrowse();
+        // get data from mapper
+        $data = (new ListData($this->model))->setInput($input)
+            ->setOption($option)
+            ->setType(ListData::PAGINATED)
+            ->toArray();
+
+        // inject data into the layout
+        $result = (new ListLayout($this->model))->setInput($input)
+            ->setOption($option)
+            ->setData($data);
+
+        return $result;
+
+        /*
+        TODO: Add i18n data
+        if (!empty($data) && isset($data['dataSource']) && isset($data['pagination'])) {
+            $layout['dataSource'] = $this->addI18nStatus($data['dataSource']);
+            $layout['meta'] = $data['pagination'];
+        } */
     }
 }
