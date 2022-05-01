635872aa65d1527e2209e5c95abeafe80c2c0815
aspirantzhang <admin@aspirantzhang.com>
2022-05-01 21:38:33 +0800

    feat: add backend global route middleware

--- app/backend/route/Admin.php
+++ app/backend/route/Admin.php
@@ -4,4 +4,4 @@ use think\facade\Route;
 
 Route::group('admins', function () {
     Route::get('', 'index');
-})->prefix('admin/')->middleware(app\middleware\RouterValidate::class, 'Admin');
+})->prefix('admin/');
