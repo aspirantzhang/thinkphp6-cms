83ded966681cf38656b2c552a11f665f46f37d4a
aspirantzhang <admin@aspirantzhang.com>
2022-05-03 21:18:42 +0800

    feat: add extend lang file used for validation

---
+++ app/backend/config/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'extend_list' => [
+        'zh-cn' => [
+            app()->getBasePath() . 'backend\lang\zh-cn\validate.php',
+        ],
+    ],
+];
