72e84a2f0ee849c723d5e122b8e08ebf302539aa
aspirantzhang <admin@aspirantzhang.com>
2022-05-04 22:00:15 +0800

    refactor: rewrite RouterValidate basic

---
+++ app/backend/lang/zh-cn/validate.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'octopus-length' => ':attribute 长度范围必须为 :rule',
+];
--- app/middleware/RouterValidate.php
+++ app/middleware/RouterValidate.php
@@ -8,16 +8,27 @@ use app\core\exception\SystemException;
 
 class RouterValidate
 {
-    public function handle($request, \Closure $next, $name)
+    public function handle($request, \Closure $next)
     {
         $appName = parse_name(app('http')->getName());
-        $validateClass = 'app\\' . $appName . '\\validate\\' . $name;
-
-        if (!class_exists($validateClass)) {
-            throw new SystemException('invalid validate class: ' . $validateClass);
+        $moduleName = $request->controller();
+        $moduleClass = 'app\\' . $appName . '\\facade\\' . $moduleName;
+        if (!class_exists($moduleClass)) {
+            throw new SystemException('invalid module class: ' . $moduleClass);
         }
+        $module = new $moduleClass();
 
-        (new $validateClass())->failException(true)->scene(parse_name($request->action()))->check($request->param());
+        $validateClass = new class ($module) extends \think\Validate {
+            public function __construct(private $module)
+            {
+                $this->setLang(app()->lang);
+                $this->scene['index'] = ['admin_name'];
+
+                $this->message['admin_name.length'] = 'octopus-length';
+            }
+        };
+
+        (new $validateClass($module))->failException(true)->scene(parse_name($request->action()))->check($request->param());
 
         return $next($request);
     }
