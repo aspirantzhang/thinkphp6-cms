2c088c41cd668517fc5e54557c08715211e5550d
aspirantzhang <admin@aspirantzhang.com>
2022-05-04 22:47:21 +0800

    fix: wrong input in getPaginatedList

--- app/backend/facade/Admin.php
+++ app/backend/facade/Admin.php
@@ -7,12 +7,13 @@ namespace app\backend\facade;
 use app\core\CoreFacade;
 use app\core\domain\Layout\ListLayout;
 use app\core\mapper\ListData;
+use think\facade\Request;
 
 class Admin extends CoreFacade
 {
     public function getPaginatedList(array $option = [], array $input = null)
     {
-        $input ??= $this->model->getAllowBrowse();
+        $input ??= Request::param($this->model->getAllowBrowse());
         // get data from mapper
         $data = (new ListData($this->model))->setInput($input)
             ->setOption($option)
