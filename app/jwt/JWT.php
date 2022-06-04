062b3ce7c28a43aec9652f8256c1a39ac3ee2f0f
aspirantzhang <admin@aspirantzhang.com>
2022-06-04 16:23:42 +0800

    feat(jwt): add jwt main class and tests

--- app/jwt/JWT.php
+++ app/jwt/JWT.php
@@ -10,19 +10,57 @@ use think\facade\Config;
 
 class JWT
 {
-    public function getToken()
-    {
-        $key = 'T3z6kcwNEJt%wXBBec_wSrpCCNhX3FFf';
+    private string $algorism = 'HS256';
+    private string $secretKey;
+    private array $claims;
 
+    public function __construct()
+    {
+        $this->init();
+    }
+
+    protected function init()
+    {
+        $this->secretKey = Config::get('jwt.key') ?? '';
+        $this->initClaims();
+    }
+
+    public function getClaims()
+    {
+        return $this->claims;
+    }
+
+    public function addClaim(string $key, mixed $value)
+    {
+        $this->claims = [...$this->claims, $key => $value];
+
+        return $this;
+    }
+
+    public function setClaim(string $key, mixed $value)
+    {
+        $this->claims[$key] = $value;
+
+        return $this;
+    }
+
+    public function initClaims()
+    {
         $now = new DateTimeImmutable();
-        $payload = [
+
+        $this->claims = [
             'iss' => Config::get('jwt.iss'),
             'aud' => Config::get('jwt.aud'),
             'iat' => $now->getTimestamp(),
             'nbf' => $now->getTimestamp(),
             'exp' => $now->modify('+ ' . (int) Config::get('jwt.exp') . ' seconds')->getTimestamp(),
         ];
+    }
 
-        return JWT_LIB::encode($payload, $key, 'HS256');
+    public function getAccessToken()
+    {
+        $payload = $this->getClaims();
+
+        return JWT_LIB::encode($payload, $this->secretKey, $this->algorism);
     }
 }
---
+++ tests/app/jwt/JWTTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\app\jwt;
+
+use app\jwt\JWT;
+use Mockery as m;
+
+class JWTTest extends \tests\TestCase
+{
+    public function setUp(): void
+    {
+        $mock = m::mock('overload:think\facade\Config');
+        $mock->shouldReceive('get')->with('jwt.key')->once()->andReturn('fake_key');
+        $mock->shouldReceive('get')->with('jwt.iss')->once()->andReturn('fake_iss');
+        $mock->shouldReceive('get')->with('jwt.aud')->once()->andReturn('fake_aud');
+        $mock->shouldReceive('get')->with('jwt.exp')->once()->andReturn('30');
+        $mock->shouldReceive('get')->with('jwt.renew')->once()->andReturn('10000');
+    }
+
+    public function testGetClaimsReturnDefaultClaims()
+    {
+        $result = (new JWT())->getClaims();
+
+        $this->assertEquals('fake_iss', $result['iss']);
+        $this->assertEquals('fake_aud', $result['aud']);
+        $this->assertEquals($result['nbf'], $result['iat']);
+        $this->assertEquals($result['iat'] + 30, $result['exp']);
+    }
+}
