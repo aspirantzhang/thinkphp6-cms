7b2bc13aa5c7d7d56af942d7bd90f4f56878a13d
aspirantzhang <admin@aspirantzhang.com>
2022-06-05 10:28:58 +0800

    refactor(jwt): replace DateTime with Carbon library

--- app/jwt/JWT.php
+++ app/jwt/JWT.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace app\jwt;
 
-use DateTimeImmutable;
+use Carbon\CarbonImmutable;
 use Firebase\JWT\JWT as JWT_LIB;
 use think\facade\Config;
 
@@ -13,7 +13,7 @@ class JWT
     private string $algorism = 'HS256';
     private string $secretKey;
     private array $claims;
-    private DateTimeImmutable $now;
+    private CarbonImmutable $now;
 
     public function __construct()
     {
@@ -22,7 +22,7 @@ class JWT
 
     protected function init()
     {
-        $this->now = new DateTimeImmutable();
+        $this->now = CarbonImmutable::now();
         $this->secretKey = Config::get('jwt.key') ?? '';
         $this->initClaims();
     }
@@ -58,7 +58,7 @@ class JWT
             'aud' => Config::get('jwt.aud'),
             'iat' => $this->now->getTimestamp(),
             'nbf' => $this->now->getTimestamp(),
-            'exp' => $this->now->modify('+ ' . (int) Config::get('jwt.exp') . ' seconds')->getTimestamp(),
+            'exp' => $this->now->addSeconds((int) Config::get('jwt.exp'))->getTimestamp(),
         ];
     }
 
@@ -71,7 +71,7 @@ class JWT
 
     public function getRefreshToken()
     {
-        $refreshExpire = $this->now->modify('+ ' . (int) Config::get('jwt.renew') . ' seconds')->getTimestamp();
+        $refreshExpire = $this->now->addSeconds((int) Config::get('jwt.renew'))->getTimestamp();
         $payload = $this->addClaim('exp', $refreshExpire)->getClaims();
 
         return JWT_LIB::encode($payload, $this->secretKey, $this->algorism);
--- tests/app/jwt/JWTTest.php
+++ tests/app/jwt/JWTTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace tests\app\jwt;
 
 use app\jwt\JWT;
+use Carbon\Carbon;
+use Carbon\CarbonImmutable;
 use Mockery as m;
 
 class JWTTest extends \tests\TestCase
@@ -15,15 +17,16 @@ class JWTTest extends \tests\TestCase
         $mock->shouldReceive('get')->with('jwt.key')->once()->andReturn('fake_key');
         $mock->shouldReceive('get')->with('jwt.iss')->once()->andReturn('fake_iss');
         $mock->shouldReceive('get')->with('jwt.aud')->once()->andReturn('fake_aud');
-        $mock->shouldReceive('get')->with('jwt.exp')->once()->andReturn('30');
-        $mock->shouldReceive('get')->with('jwt.renew')->once()->andReturn('10000');
+        $mock->shouldReceive('get')->with('jwt.exp')->once()->andReturn(30);
+        $mock->shouldReceive('get')->with('jwt.renew')->once()->andReturn(10000);
+        CarbonImmutable::setTestNow(Carbon::parse(1600000000));
     }
 
     public function testGetAddClaim()
     {
-        $refreshExpire = 9999;
+        $refreshExpire = 1600009999;
         $result = (new JWT())->addClaim('exp', $refreshExpire)->addClaim('foo', 'bar')->getClaims();
-        $this->assertEquals(9999, $result['exp']);
+        $this->assertEquals(1600009999, $result['exp']);
         $this->assertEquals('bar', $result['foo']);
     }
 
@@ -40,7 +43,7 @@ class JWTTest extends \tests\TestCase
     public function testGetClaimForDefaultValue()
     {
         $result = (new JWT())->getClaim('exp');
-        $this->assertEquals(time() + 30, $result);
+        $this->assertEquals(1600000030, $result);
     }
 
     public function testGetClaimForExtraValue()
