a3319fc7eba49b1bef35dd44e782303547be42fb
aspirantzhang <admin@aspirantzhang.com>
2022-06-08 21:04:09 +0800

    feat(jwt): add getToken method for JWT

--- app/jwt/JWT.php
+++ app/jwt/JWT.php
@@ -4,6 +4,20 @@ declare(strict_types=1);
 
 namespace app\jwt;
 
+use app\jwt\token\AccessToken;
+use app\jwt\token\RefreshToken;
+use app\jwt\token\Token;
+
 class JWT
 {
+    public function getToken()
+    {
+        $accessToken = (new Token(new AccessToken()))->getToken();
+        $refreshToken = (new Token(new RefreshToken()))->getToken();
+
+        return [
+            'access_token' => $accessToken,
+            'refresh_token' => $refreshToken,
+        ];
+    }
 }
