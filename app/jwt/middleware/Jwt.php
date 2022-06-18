308cc909712e1e2f85161abfecaa3a5b06236b3c
aspirantzhang <admin@aspirantzhang.com>
2022-06-18 10:15:33 +0800

    feat(jwt): improve response method for jwt middleware

--- app/jwt/middleware/Jwt.php
+++ app/jwt/middleware/Jwt.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace app\jwt\middleware;
 
+use app\core\view\JsonView;
 use app\jwt\exception\TokenExpiredException;
 use app\jwt\exception\TokenInvalidException;
-use think\Response;
 
 class Jwt
 {
@@ -18,11 +18,11 @@ class Jwt
         }
 
         try {
-            $payload = app('jwt')->checkAccessToken($request);
+            app('jwt')->checkAccessToken($request);
         } catch (TokenExpiredException) {
-            return Response::create(error(message: 'token expired', code: 401), 'json', 401);
+            return (new JsonView(error(message: 'token expired', code: 401)))->output();
         } catch (TokenInvalidException) {
-            return Response::create(error(message: 'invalid token', code: 401), 'json', 401);
+            return (new JsonView(error(message: 'invalid token', code: 401)))->output();
         }
 
         return $next($request);
