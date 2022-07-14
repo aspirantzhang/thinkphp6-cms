33a5fb29b54323ced157d06bd39eeced3d93b035
aspirantzhang <admin@aspirantzhang.com>
2022-07-14 21:18:28 +0800

    refactor: improve route rule order for Admin

--- app/backend/route/Admin.php
+++ app/backend/route/Admin.php
@@ -3,11 +3,13 @@
 use think\facade\Route;
 
 Route::group('admins', function () {
+    Route::post('refresh-token', 'refreshToken');
+    Route::post('login', 'login');
+    Route::get('add', 'add');
+
     Route::post(':id', 'update');
     Route::get(':id', 'view');
-    Route::get('add', 'add');
-    Route::post('login', 'login');
+
     Route::get('', 'index');
     Route::post('', 'store');
-    Route::post('/refresh-token', 'refreshToken');
 })->prefix('admin/');
